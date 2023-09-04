e00ff215de1b71310919e86313fe8e1c9ac8ed9f
tumble <tumblegamer@gmail.com>
2023-09-04 12:00:55 +0100

    oki commented

--- denki/iframe.js
+++ denki/iframe.js
@@ -1,6 +1,15 @@
 window.addEventListener('message', (event) => {
-	if (event.data.length < 2) return;
-	if (typeof event.data[0] !== "string") return;
-	if (typeof event.data[1] !== "object") return;
-	document.dispatchEvent(new KeyboardEvent(...event.data));
+	if (!Array.isArray(event.data) || event.data.length < 2) {
+		// Check if event.data is an array with at least two elements
+		return;
+	}
+
+	const [eventType, eventOptions] = event.data;
+
+	if (typeof eventType !== "string" || typeof eventOptions !== "object") {
+		// Check if the first element is a string and the second is an object
+		return;
+	}
+
+	document.dispatchEvent(new KeyboardEvent(eventType, eventOptions));
 });
