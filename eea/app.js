3e3f5281433fbcce10cc8b1ccd0772907832f512
Tumble <tumble1999@users.noreply.github.com>
2023-10-27 18:59:06 +0100

    remove mouse binds for eea

--- eea/app.js
+++ eea/app.js
@@ -1,4 +1,3 @@
 loadSWF("app.swf",
 [{"button":"help","keys":["h"],"codes":["KeyH"],"keyCodes":[72]}],
-{"main":{"select":{"target":"select","left":0.30541665818956165,"right":0.926249991522895,"top":0.020138899485270183,"bottom":0.08263889948527019,"role":"slider-horizontal"}}}
 )
